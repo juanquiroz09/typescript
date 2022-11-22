8a08a26cb69fdc3c17d894e9eb15ace97d8235c4
Juan Quiroz <juan.quiroz1009@gmail.com>
2022-11-22 13:03:50 -0500

    second-commit

--- coche.js
+++ coche.js
@@ -1,7 +1,20 @@
 var Coche = /** @class */ (function () {
-    function Coche() {
+    function Coche(modelo) {
+        if (modelo === void 0) { modelo = null; }
         this.velocidad = 0;
+        if ((modelo = null)) {
+            this.modelo = "BMW Generico";
+        }
+        else {
+            this.modelo = modelo;
+        }
     }
+    Coche.prototype.getMOdelo = function () {
+        return this.modelo;
+    };
+    Coche.prototype.setModelo = function (modelo) {
+        this.modelo = modelo;
+    };
     Coche.prototype.getColor = function () {
         return this.color;
     };
@@ -26,7 +39,10 @@ coche.setColor("Rojo");
 coche.acelerar();
 coche.acelerar();
 coche.acelerar();
+console.log("El modelo del coche 1 es: " + coche.getMOdelo());
 console.log("El color del coche 1 es: " + coche.getColor());
+console.log("La velocidad del coche 1 es: " + coche.getVelocidad());
+coche.frenar();
 console.log("El color del coche 1 es: " + coche.getVelocidad());
 /*
 coche_dos.setColor("Azul");
@@ -34,4 +50,4 @@ coche_tres.setColor("Verde");
 
 console.log("El color del coche 2 es: " + coche_dos.getColor());
 console.log("El color del coche 3 es: " + coche_tres.getColor());
-*/ 
+*/
--- coche.ts
+++ coche.ts
@@ -1,23 +1,37 @@
 class Coche {
   public color: string;
   public modelo: string;
-  public velocidad: number = 0;
+  public velocidad: number;
 
-  public getColor(){
+  constructor(modelo: any = null) {
+    this.velocidad = 0;
+    if ((modelo = null)) {
+      this.modelo = "BMW Generico";
+    } else {
+        this.modelo = modelo;
+    }
+  }
+  getMOdelo(){
+    return this.modelo;
+  }
+  setModelo(modelo: string){
+    this.modelo = modelo;
+  }
+
+  public getColor() {
     return this.color;
   }
 
-  public setColor(color: string){
+  public setColor(color: string) {
     this.color = color;
-
   }
-  public acelerar(){
+  public acelerar() {
     this.velocidad++;
   }
-  public frenar(){
+  public frenar() {
     this.velocidad--;
   }
-  public getVelocidad():number{
+  public getVelocidad(): number {
     return this.velocidad;
   }
 }
@@ -30,8 +44,13 @@ coche.setColor("Rojo");
 coche.acelerar();
 coche.acelerar();
 coche.acelerar();
+console.log("El modelo del coche 1 es: " + coche.getMOdelo());
 console.log("El color del coche 1 es: " + coche.getColor());
+console.log("La velocidad del coche 1 es: " + coche.getVelocidad());
+
+coche.frenar();
 console.log("El color del coche 1 es: " + coche.getVelocidad());
+
 /*
 coche_dos.setColor("Azul");
 coche_tres.setColor("Verde");
